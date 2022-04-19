e646786f723729ef8772e7c7348720dc55d6643a
ismail abanoz <ismailabanoz1213@gmail.com>
2022-04-20 01:59:53 +0300

    first commit

---
+++ mert_software_task.WebFormsUI/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace mert_software_task.WebFormsUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Umuttttt");
+        }
+    }
+}
