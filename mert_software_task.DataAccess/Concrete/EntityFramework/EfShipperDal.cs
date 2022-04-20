2497c005f39d51413a744d90d628d2292d3eb633
ismail abanoz <ismailabanoz1213@gmail.com>
2022-04-21 00:57:54 +0300

    Added concrete objects for data access layer

---
+++ mert_software_task.DataAccess/Concrete/EntityFramework/EfShipperDal.cs
@@ -0,0 +1,13 @@
+﻿using mert_software_task.DataAccess.Abstract;
+using mert_software_task.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace mert_software_task.DataAccess.Concrete.EntityFramework
+{
+    public class EfShipperDal : EfEntityRepositoryBase<Shipper, mert_software_taskContext>, IShipper
+    {
+    }
+}
